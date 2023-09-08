5170dda3c6889fb27ac525858161eb810fad1f7f
nicolau-np <mr1Normaliii@gmail.com>
2023-09-08 10:21:23 +0100

    update

--- js/script.js
+++ js/script.js
@@ -3,36 +3,76 @@ const next = document.querySelector("#next");
 
 const validateDate = document.querySelector(".validate-date .date");
 
-const listOfCards = document.querySelectorAll('.card');
-const cardNumber = document.querySelectorAll('.card-number span');
+const listOfCards = document.querySelectorAll(".card");
+const cardNumber = document.querySelector(".card-number span");
+
+const cardNumberInput = document.querySelector("input[name='card_number']");
+const validThruInput = document.querySelector("input[name='valid_thru']");
 
 var active = 0;
 
 showCardSide();
 setValues();
 
+validThruInput.addEventListener("keyup", e => {
+  if (validThruInput.value.length === 0) {
+    valueDefaultValidateDate();
+  } else {
+    if (validThruInput.value.length <= 7) {
+      validateDate.textContent = validThruInput.value;
+    }
+  }
+});
 
+cardNumberInput.addEventListener("keyup", e => {
+  if (cardNumberInput.value.length === 0) {
+    valueDefaultCardNumber();
+  } else {
+    if (cardNumberInput.value.length <= 16) {
+      formatNumber();
+    }
+  }
+});
 
-function setValues(){
-    cardNumber.forEach((element)=>{
-        element.textContent = "0000";
-    })
-    validateDate.textContent = "01/2023";
+function formatNumber() {
+  numero = cardNumberInput.value;
+
+  let numeroFormatado = "";
+  for (let i = 0; i < numero.length; i++) {
+    if (i > 0 && i % 4 === 0) {
+      numeroFormatado += "  ";
+    }
+    numeroFormatado += numero.charAt(i);
+  }
+  cardNumber.textContent = numeroFormatado;
 }
 
-function showCardSide(){
-    listOfCards.forEach((element)=>{
-        element.classList.remove('active')
-    });
-    listOfCards[active].classList.add('active');
+function setValues() {
+  valueDefaultCardNumber();
+  valueDefaultValidateDate();
 }
 
-prev.addEventListener('click', e=>{
-   active = parseInt(prev.getAttribute('data-index'))
-    showCardSide();
-})
+function valueDefaultValidateDate() {
+  validateDate.textContent = "01/2023";
+}
 
-next.addEventListener('click', e=>{
-    active = parseInt(next.getAttribute('data-index'))
-    showCardSide();
-})
+function valueDefaultCardNumber() {
+  cardNumber.textContent = "0000  0000  0000  0000";
+}
+
+function showCardSide() {
+  listOfCards.forEach(element => {
+    element.classList.remove("active");
+  });
+  listOfCards[active].classList.add("active");
+}
+
+prev.addEventListener("click", e => {
+  active = parseInt(prev.getAttribute("data-index"));
+  showCardSide();
+});
+
+next.addEventListener("click", e => {
+  active = parseInt(next.getAttribute("data-index"));
+  showCardSide();
+});
